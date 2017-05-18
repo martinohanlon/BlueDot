1429dc6efce7072f875c878275435315d184d619
Python3pkg <raliclo@gmail.com>
2017-05-17 23:14:30 -0700

    Convert to python3

--- examples/slider_horizontal.py
+++ examples/slider_horizontal.py
@@ -4,7 +4,7 @@ from signal import pause
 def show_percentage(pos):
     horizontal = ((pos.x + 1) / 2)
     percentage = round(horizontal * 100, 2)
-    print("{}%".format(percentage))
+    print(("{}%".format(percentage)))
 
 bd = BlueDot()
 bd.when_moved = show_percentage
