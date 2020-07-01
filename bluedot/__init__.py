e7aa7eeffefa945c575a0fd26e889f7c4ba8bb1b
Martin O'Hanlon <martin.ohanlon@raspberrypi.org>
2020-07-01 22:12:39 +0100

    created interactions module

--- bluedot/__init__.py
+++ bluedot/__init__.py
@@ -1,3 +1,4 @@
-from .dot import BlueDot, BlueDotPosition, BlueDotInteraction, BlueDotSwipe, BlueDotRotation
+from .dot import BlueDot
+from .interactions import BlueDotInteraction, BlueDotPosition, BlueDotRotation, BlueDotSwipe
 from .colors import COLORS
 from .mock import MockBlueDot
