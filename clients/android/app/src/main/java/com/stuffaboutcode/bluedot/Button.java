18dd8a088b57e18a6314207de59a769a604759b1
Martin O'Hanlon <martin.ohanlon@raspberrypi.org>
2020-08-12 14:46:43 +0100

    fix app issue

--- clients/android/app/src/main/java/com/stuffaboutcode/bluedot/Button.java
+++ clients/android/app/src/main/java/com/stuffaboutcode/bluedot/Button.java
@@ -241,6 +241,15 @@ public class Button extends AppCompatActivity {
             if (parameters[4].equals("0")) {
                 matrix.setVisible(false);
             } else {
+
+                // cols
+                if (!parameters[5].equals(""))
+                    matrix.setCols(Integer.parseInt(parameters[5]));
+
+                // rows
+                if (!parameters[6].equals(""))
+                    matrix.setRows(Integer.parseInt(parameters[6]));
+
                 //color
                 if (!parameters[1].equals("")) {
                     String color = convertColor(parameters[1]);
@@ -263,13 +272,6 @@ public class Button extends AppCompatActivity {
                 if (!parameters[4].equals(""))
                     matrix.setVisible(parameters[4].equals("1"));
 
-                // cols
-                if (!parameters[5].equals(""))
-                    matrix.setCols(Integer.parseInt(parameters[5]));
-
-                // rows
-                if (!parameters[6].equals(""))
-                    matrix.setRows(Integer.parseInt(parameters[6]));
             }
 
             matrix.update();
